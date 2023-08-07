1765dec4ba2e9b80145ba4160f5bd1804a866e66
manishauc <39763426+manishauc@users.noreply.github.com>
2023-08-07 18:28:01 +0530

    Update myblog-ddl-script.sql

--- springboot-blog-rest-api/myblog-ddl-script.sql
+++ springboot-blog-rest-api/myblog-ddl-script.sql
@@ -1,42 +1,4 @@
-DROP TABLE IF EXISTS `posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `posts` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `content` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UKmchce1gm7f6otpphxd6ixsdps` (`title`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-DROP TABLE IF EXISTS `comments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `comments` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `body` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `post_id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKh4c7lvsc298whoyd4w9ta25cr` (`post_id`),
-  CONSTRAINT `FKh4c7lvsc298whoyd4w9ta25cr` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UKr43af9ap4edm43mmtq01oddj6` (`username`),
-  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE DATABASE twitter_app_db;
 
 DROP TABLE IF EXISTS `roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -46,6 +8,10 @@ CREATE TABLE `roles` (
   `name` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `twitter_app_db`.`roles` (`id`, `name`) VALUES ('1', 'ROLE_USER');
+INSERT INTO `twitter_app_db`.`roles` (`id`, `name`) VALUES ('2', 'ADMIN');
+
 
 DROP TABLE IF EXISTS `user_roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
